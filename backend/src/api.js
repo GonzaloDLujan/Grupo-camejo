89d16aa9f0cdbd4be87f4285f6a4742e139069c0
Gonzalo Lujan <glujan@fi.uba.ar>
2025-12-10 18:51:19 -0300

    Agrego el Delete y Post

--- backend/src/api.js
+++ backend/src/api.js
@@ -29,18 +29,28 @@ app.get('/api/agentes', async (req, res) => {
 })
 
 //Ver un Agente
-app.get('/api/agentes/:id', async(req, res) => {
+app.get('/api/agentes/:id', async (req, res) => {
   const agente = await getOneAgente(req.params.id)
   if (!agente) {
-    return res.status(404).json({ error:'Agente No Encontrado'})
+    return res.status(404).json({ error: 'Agente No Encontrado' })
   } else {
     return res.json(agente)
   }
 })
 
 //Insertar Agente
-app.post('/api/agentes', (req, res) => {
+app.post('/api/agentes', async (req, res) => {
 
+  if (!req.params.nombre || !req.params.especie || !req.params.fecha_de_ingreso || !req.params.estado || !req.params.nivel_de_habilidad) {
+    return res.status(400).json({ error: "Missing Required Fields" })
+  }
+  const agente = await postAgente(req.params.nombre, req.params.especie, req.params.fecha_de_ingreso, req.params.estado, req.params.nivel_de_habilidad)
+
+  if (!agente) {
+    return res.status(500).json({ error: 'Failed To Create Agente' })
+  } else {
+    return res.json(agente)
+  }
 })
 
 //Editar Agente
@@ -49,6 +59,11 @@ app.put('/api/agentes', (req, res) => {
 })
 
 //Borrar Agente
-app.delete('/api/agentes/:id', (req, res) => {
-
+app.delete('/api/agentes/:id', async (req, res) => {
+  const agente = await deleteAgente(req.params.id)
+  if (!agente) {
+    return res.status(404).json({ error: 'Agente id: ' + req.params.id + ' No Encontrado' })
+  } else {
+    return res.json({ status: 'OK', id: agente })
+  }
 })
--- backend/src/db.js
+++ backend/src/db.js
@@ -4,35 +4,52 @@ import { Pool } from pg
 
 //Conexion con la DB
 const dbClient = new Pool({
-    user: 'postgres',
+    user: 'postgresult',
     port: 5432,
     host: 'localhost',
     database: 'OWCA',
-    password: 'postgres'
+    password: 'postgresult'
 });
 
-
+//--------AGENTES---------------
 async function getAllAgentes() {
-    const res = await dbClient.query('SELECT * FROM agentes')
-    return res.rows
+    const result = await dbClient.query('SELECT * FROM agentes')
+    return result.rows
 }
 
 async function getOneAgente(id) {
-    const res = await dbClient.query('SELECT * FROM agentes WHERE id = $1 LIMIT 1', [id])
-    return res.rows[0]
+    const result = await dbClient.query('SELECT * FROM agentes WHERE id = $1 LIMIT 1', [id])
+    return result.rows[0]
+}
+
+async function postAgente(nombre, especie, fecha_de_ingresulto, estado, nivel_de_habilidad) {
+    const result = await dbClient.query('INSERTO INTO agentes(nombre, especie, fecha_de_ingresulto, estado, nivel_de_habilidad) VALUES ($1, $2, $3, $4, $5) RETURNING *', [nombre, especie, fecha_de_ingresulto, estado, nivel_de_habilidad])
+
+    if (result.rowCount === 0) {
+        return undefined
+    } else {
+        return result.rows[0]
+    }
 }
 
 async function putAgente(id) {
 
 }
 
-async function postAgente() {
-
-}
-
 async function deleteAgente(id) {
-
+    const result = await dbClient.query('DELETE FROM agentes WHERE id = $1', [id])
+    if (result.rowCount === 0) {
+        return undefined
+    } else {
+        return id
+    }
 }
+
+//------------MISIONES------------------
+
+//------------VILLANOS------------------
+
+
 
 module.exports = {
     getAllAgentes,
