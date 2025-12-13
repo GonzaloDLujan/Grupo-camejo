db032e8fef3a50d2baac231fb662b1da6b8fece8
Franco Campos <fecampos@fi.uba.ar>
2025-12-13 20:38:22 -0300

    Agrego endpoints villanos y misiones

--- backend/src/api.js
+++ backend/src/api.js
@@ -5,7 +5,17 @@ import {
   getOneAgente,
   putAgente,
   postAgente,
-  deleteAgente
+  deleteAgente,
+  getAllVillanos,
+  getOneVillano,
+  postVillano,
+  putVillano,
+  deleteVillano,
+  getAllMisiones,
+  getOneMision,
+  postMision,
+  putMision, 
+  deleteMision
 } from "db"
 
 const app = express()
@@ -66,4 +76,288 @@ app.delete('/api/agentes/:id', async (req, res) => {
   } else {
     return res.json({ status: 'OK', id: agente })
   }
+})
+
+//END POINTS VILLANOS
+
+//VER TODOS LOS VILLANOS
+app.get('/api/villanos', async (req, res) => {
+  const villanos = await getAllVillanos();
+  res.json(villanos);
+});
+
+//VER UN VILLANO
+app.get('/api/villanos/:id', async (req, res) => {
+  const villano = await getOneVillano(req.params.id);
+
+  if (villano === undefined) {
+    res.sendStatus(404);
+  }
+
+  res.json(villano);
+});
+
+//CREAR UN VILLANO
+app.post('/api/villanos', async (req, res) => {
+    if (req.body === undefined) {
+        return res.status(400).send("No body was provided");
+    }
+    
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const ocupacion = req.body.ocupacion;
+    const ubicacion = req.body.ubicacion;
+    const estado = req.body.estado;
+    const apodo = req.body.apodo;
+
+    if ((await getOneVillano(id)) !== undefined) {
+        return res.status(409).send("The villian already exists");
+    }
+
+    if (nombre === undefined) {
+        return res.status(400).send("Name not provided");
+    }
+
+    if (edad === undefined) {
+        return res.status(400).send("Age not provided");
+    }
+
+    if (ocupacion === undefined) {
+        return res.status(400).send("Occupation not provided");
+    }
+
+    if (ubicacion === undefined) {
+        return res.status(400).send("Location not provided");
+    }
+
+    if (estado === undefined) {
+        return res.status(400).send("Status not provided");
+    }
+
+    if (apodo === undefined) {
+        return res.status(400).send("Nickname not provided");
+    }
+
+    const villano = await createCousin(nombre, edad, ocupacion, ubicacion, estado, apodo);
+
+    res.status(201).json(villano);
+});
+
+//EDITAR VILLANO
+app.put('/api/villanos/:id', async (req, res) => {
+  let villano = await getOneVillano(req.params.id);
+
+  if (villano === undefined) {
+    return res.sendStatus(404);
+  }
+
+  if (req.body === undefined) {
+        return res.status(400).send("No body was provided");
+    }
+
+    const nombre = req.body.nombre;
+    const edad = req.body.edad;
+    const ocupacion = req.body.ocupacion;
+    const ubicacion = req.body.ubicacion;
+    const estado = req.body.estado;
+    const apodo = req.body.apodo;
+
+    if (nombre === undefined) {
+        return res.status(400).send("Name not provided");
+    }
+
+    if (edad === undefined) {
+        return res.status(400).send("Age not provided");
+    }
+
+    if (ocupacion === undefined) {
+        return res.status(400).send("Occupation not provided");
+    }
+
+    if (ubicacion === undefined) {
+        return res.status(400).send("Location not provided");
+    }
+
+    if (estado === undefined) {
+        return res.status(400).send("Status not provided");
+    }
+
+    if (apodo === undefined) {
+        return res.status(400).send("Nickname not provided");
+    }
+
+    villano = await putVillano(nombre, edad, ocupacion, ubicacion, estado, apodo);
+
+    if (villano === undefined) {
+        return res.sendStatus(500);
+    }
+
+    res.json(villano);
+});
+
+//ELIMINAR VILLANO
+app.delete('/api/villanos/:id', async (req, res) => {
+  const villano = await getOneVillano(req.params.id);
+
+  if (villano === undefined) {
+    return res.sendStatus(404);
+  }
+
+  if(!(await deleteVillano(req.params.id))) {
+    return res.sendStatus(500);
+  }
+
+  return res.json(villano);
+})
+
+//END POINTS MISIONES
+
+//VER TODAS LAS MISIONES 
+app.get('/api/misiones', async (req, res) => {
+  const misiones = await getAllMisiones();
+  return res.json(misiones);
+});
+
+//VER UNA MISION
+app.get('/api/misiones/:id', async (req, res) => {
+  const mision = await getOneMision(req.params.id);
+
+  if (mision === undefined) {
+    return res.sendStatus(404);
+  }
+
+  return res.json(mision);
+})
+
+//CREAR UNA MISION
+app.post('/api/misiones', async (req, res) => {
+  if (req.body === undefined) {
+        return res.status(400).send("No body was provided");
+    }
+    
+    const id_agente = req.body.id_agente;
+    const id_villano = req.body.id_villano;
+    const fecha = req.body.fecha;
+    const titulo = req.body.titulo;
+    const descripcion = req.body.descripcion;
+    const estado = req.body.estado;
+    const coste = req.body.coste;
+    const dificultad = req.body.dificultad;
+
+    if ((await getOneMision(id)) !== undefined) {
+        return res.status(409).send("The mission already exists");
+    }
+
+    if (id_agente === undefined) {
+        return res.status(400).send("Agent ID not provided");
+    }
+
+    if (id_villano === undefined) {
+        return res.status(400).send("Villain ID not provided");
+    }
+
+    if (fecha === undefined) {
+        return res.status(400).send("Date not provided");
+    }
+
+    if (titulo === undefined) {
+        return res.status(400).send("Title not provided");
+    }
+
+    if (descripcion === undefined) {
+        return res.status(400).send("Description not provided");
+    }
+
+    if (estado === undefined) {
+        return res.status(400).send("Status not provided");
+    }
+
+    if (coste === undefined) {
+        return res.status(400).send("Cost not provided");
+    }
+
+    if (dificultad === undefined) {
+        return res.status(400).send("Difficulty  not provided");
+    }
+
+    const mision = await postMision(id_agente, id_villano, fecha, titulo, descripcion, estado, coste, dificultad);
+
+    res.status(201).json(mision);
+})
+
+//EDITAR UNA MISION
+get.put('/api/misiones/:id', async (req, res) => {
+  let mision = await getOneMision(req.params.id);
+
+  if (mision === undefined) {
+    return res.sendStatus(404);
+  }
+
+  if (req.body === undefined) {
+        return res.status(400).send("No body was provided");
+    }
+
+    const id_agente = req.body.id_agente;
+    const id_villano = req.body.id_villano;
+    const fecha = req.body.fecha;
+    const titulo = req.body.titulo;
+    const descripcion = req.body.descripcion;
+    const estado = req.body.estado;
+    const coste = req.body.coste;
+    const dificultad = req.body.dificultad;
+
+    if (id_agente === undefined) {
+        return res.status(400).send("Agent ID not provided");
+    }
+
+    if (id_villano === undefined) {
+        return res.status(400).send("Villain ID not provided");
+    }
+
+    if (fecha === undefined) {
+        return res.status(400).send("Date not provided");
+    }
+
+    if (titulo === undefined) {
+        return res.status(400).send("Title not provided");
+    }
+
+    if (descripcion === undefined) {
+        return res.status(400).send("Description not provided");
+    }
+
+    if (estado === undefined) {
+        return res.status(400).send("Status not provided");
+    }
+
+    if (coste === undefined) {
+        return res.status(400).send("Cost not provided");
+    }
+
+    if (dificultad === undefined) {
+        return res.status(400).send("Difficulty  not provided");
+    }
+
+    mision = await putMision(id_agente, id_villano, fecha, titulo, descripcion, estado, coste, dificultad);
+
+    if (mision === undefined) {
+        return res.sendStatus(500);
+    }
+
+    res.json(mision);
+});
+
+//ELIMINAR MISION
+app.delete('/api/misiones/:id', async (req, res) => {
+  const mision = await getOneMision(req.params.id);
+
+  if (mision === undefined) {
+    return res.sendStatus(404);
+  }
+
+  if(!(await deleteMision(req.params.id))) {
+    return res.sendStatus(500);
+  }
+
+  return res.json(mision);
 })
--- backend/src/db.js
+++ backend/src/db.js
@@ -12,6 +12,7 @@ const dbClient = new Pool({
 });
 
 //--------AGENTES---------------
+
 async function getAllAgentes() {
     const result = await dbClient.query('SELECT * FROM agentes')
     return result.rows
@@ -22,7 +23,7 @@ async function getOneAgente(id) {
     return result.rows[0]
 }
 
-async function postAgente(nombre, especie, fecha_de_ingresulto, estado, nivel_de_habilidad) {
+async function postAgente(nombre, especie, fecha_de_ingreso, estado, nivel_de_habilidad) {
     const result = await dbClient.query('INSERTO INTO agentes(nombre, especie, fecha_de_ingresulto, estado, nivel_de_habilidad) VALUES ($1, $2, $3, $4, $5) RETURNING *', [nombre, especie, fecha_de_ingresulto, estado, nivel_de_habilidad])
 
     if (result.rowCount === 0) {
@@ -47,9 +48,137 @@ async function deleteAgente(id) {
 
 //------------MISIONES------------------
 
+async function getAllMisiones() {
+    const result = await dbClient.query('SELECT * FROM misiones');
+    return result.rows;
+}
+
+async function getOneMision(id) {
+    const result = await dbClient.query('SELECT * FROM misiones WHERE id = $1', [id]);
+    return result.rows[0];
+}
+
+async function postMision(id_agente, id_villano, fecha, titulo, descripcion, estado, coste, dificultad) {
+    const result = await dbClient.query(
+        'INSERT INTO misiones (id_agente, id_villano, fecha, titulo, descripcion, estado, coste, dificultad) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *',
+        [id_agente, id_villano, fecha, titulo, descripcion, estado, coste, dificultad]);
+    
+    if (result.rowCount === 0) {
+        return undefined;
+    }
+
+    return {
+        id_agente: result.rows[0].id_agente,
+        id_villano: result.rows[0].id_villano,
+        fecha: result.rows[0].fecha,
+        titulo: result.rows[0].titulo,
+        descripcion: result.rows[0].descripcion,
+        estado: result.rows[0].estado,
+        coste: result.rows[0].coste,
+        dificultad: result.rows[0].dificultad
+    };
+}
+
+async function putMision(id, id_agente, id_villano, fecha, titulo, descripcion, estado, coste, dificultad) {
+    const result = await dbClient.query(
+        'UPDATE misiones SET id_agente = $2, id_villano = $3, fecha = $4, titulo = $5, descripcion = $6, estado = $7, coste = $8, dificultad = $9 WHERE id = $1',
+    [id, id_agente, id_villano, fecha, titulo, descripcion, estado, coste, dificultad]);
+
+    if (result.rowCount === 0) {
+        return undefined;
+    }
+
+    return {
+        id: id,
+        id_agente: id_agente,
+        id_villano: id_villano,
+        fecha: fecha,
+        titulo: titulo,
+        descripcion: descripcion,
+        estado: estado,
+        coste: coste,
+        dificultad: dificultad
+    }
+}
+
+async function deleteMision(id) {
+    const result = await dbClient.query('DELETE FROM misiones WHERE id = $1', [id]);
+
+    if (result.rowCount === 0) {
+        return undefined;
+    }
+
+    return id;
+}
+
 //------------VILLANOS------------------
 
+async function getAllVillanos() {
+    const result = await dbClient.query('SELECT * FROM villanos;')
+    return result.rows;
+}
 
+async function getOneVillano(id) {
+    const result = await dbClient.query('SELECT * FROM villanos WHERE id = $1;', [id])
+
+    if (result.rows.length === 0) {
+        return undefined;
+    }
+
+    return result.rows[0];
+}
+
+async function postVillano(nombre, edad, ocupacion, ubicacion, estado, apodo) {
+    const result = await dbClient.query(
+        'INSERT INTO villanos (nombre, edad, ocupacion, ubicacion, estado, apodo) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *',
+        [nombre, edad, ocupacion, ubicacion, estado, apodo]);
+    
+    if (result.rowCount === 0) {
+        return undefined;
+    }
+
+    return {
+        id: result.rows[0].id,
+        nombre: result.rows[0].nombre,
+        edad: result.rows[0].edad,
+        ocupacion: result.rows[0].ocupacion,
+        ubicacion: result.rows[0].ubicacion,
+        estado: result.rows[0].estado,
+        apodo: result.rows[0].apodo
+    };
+}
+
+async function putVillano(id, nombre, edad, ocupacion, ubicacion, estado, apodo) {
+    const result = await dbClient.query(
+        'UPDATE villanos SET nombre = $2, edad = $3, ocupacion = $4, ubicacion = $5, estado = $6, apodo = $7 WHERE id = $1',
+        [id, nombre, edad, ocupacion, ubicacion, estado, apodo]);
+    
+    if (result.rowCount === 0) {
+        return undefined;
+    }
+
+    return {
+        id: id,
+        nombre: nombre,
+        edad: edad,
+        ocupacion: ocupacion,
+        ubicacion: ubicacion,
+        estado: estado,
+        apodo: apodo
+    };
+}
+
+async function deleteVillano(id) {
+    const resut = await dbClient.query('DELETE FROM villanos WHERE id = $1', [id]);
+
+    if (result.rowCount === 0) {
+        return undefined;
+    }
+
+    return id;
+}
+
+//-------EXPORTACION DE FUNCIONES-------
 
 module.exports = {
     getAllAgentes,
